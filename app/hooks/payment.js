622fccb5bf7297f333196d30904025bf275cbd69
Shawn Hu <theseus.hu@zoho.com>
2017-05-29 02:02:53 +0800

    test commit

--- app/hooks/payment.js
+++ app/hooks/payment.js
@@ -2,4 +2,7 @@ export default (app) => {
   app.get('/test', (req, res) => {
     res.send('hello world');
   });
+  app.post('/hooks/chargeSucceed', (req, res) => {
+    res.send('hello world');
+  });
 };
